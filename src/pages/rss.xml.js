1842b9a65633b32c83241434343554048efea8ae
luv nft <info@blkluv.org>
2023-12-01 13:26:16 -0500

    done

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss from "@astrojs/rss";
 export const get = () =>
 	rss({
 		title: "LUV NFT",
-		description: "Personal blog for LUV NFT",
+		description: "Y LUV NFT",
 		site: import.meta.env.SITE,
 		items: import.meta.glob("./blog/**/*.{md,mdx}"),
 	});
